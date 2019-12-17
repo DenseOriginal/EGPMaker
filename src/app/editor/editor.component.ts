cf8d31806389612fd0c65253ed83dbd110fc90ae
DenseOriginal <48748922+DenseOriginal@users.noreply.github.com>
2019-12-17 11:06:08 +0100

    Added objectstack

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as p5 from 'p5';
+import { IShape, EGPShapes } from '../shared/interfaces';
+
+// Global variables, so that both EditorComponent and the p5js sketch can use them
+// Can't find out how to pass variables betweem them other that this...
+// Kinda gross, but whatever
+var objectStack: IShape[] = []; // Stack to hold all the objects drawn to the screen
 
 @Component({
   selector: 'app-editor',
@@ -37,16 +43,36 @@ export class EditorComponent implements OnInit, OnDestroy {
   private editorSketch(p: p5) {
     let tempObject = <any>{} // Remove later
     var mouse = {x: 0, y: 0}; // Object to contain the x, y position of the mouse
+    var canvas; // p5 canvas
+    var _this: EditorComponent;
 
     p.setup = () => {
-      p.createCanvas(window.innerWidth / 2.5, window.innerWidth / 2.5).parent('editor-canvas');
+      canvas = p.createCanvas(window.innerWidth / 2.5, window.innerWidth / 2.5).parent('editor-canvas');
+      canvas.style('border', 'solid 0.5px black');
       p.angleMode(p.DEGREES);
       p.background(255, 175, 175);
     };
 
     p.draw = () => {
       p.background(255);
+      p.strokeWeight(0.5);
 
+      // Draw every object in the object stack
+      objectStack.forEach(object => {
+        p.fill(object.color.r, object.color.g, object.color.b, object.color.a); // Set fill color to the object color
+        
+        switch (object.objectData.type) {
+          case 'box':
+            p.rect(object.position[0].x, object.position[0].y, object.position[1].x, object.position[1].y)
+            break;
+        
+          default:
+            break;
+        }
+      });
+
+      // Draw tempObject on top of every thing else
+      // Replace with something other
       if(tempObject.pos1) {
         if(tempObject.pos2) {
           p.rect(tempObject.pos1.x, tempObject.pos1.y, tempObject.pos2.x, tempObject.pos2.y);
@@ -64,11 +90,9 @@ export class EditorComponent implements OnInit, OnDestroy {
       // Should happen before any other mouse function
       mouse.x = Math.min(Math.max(p.mouseX, 0), p.width); // Limits mouse.x to inside the screen
       mouse.y = Math.min(Math.max(p.mouseY, 0), p.height); // Limits mouse.y to inside the screen
-      console.log(3)
     }
 
     p.mousePressed = () => {
-      console.log(1);
       if(p.mouseX > p.width || p.mouseX < 0 || p.mouseY > p.height || p.mouseY < 0) return;
       if(!tempObject.pos1) {
         tempObject.pos1 = {
@@ -85,12 +109,35 @@ export class EditorComponent implements OnInit, OnDestroy {
     }
 
     p.mouseReleased = () => {
-      console.log(2);
+      if(!tempObject.pos1) return; // Escpae the function if tempObject is undefined
+      console.log(tempObject);
       tempObject.pos2 = {
         x: mouse.x - tempObject.pos1.x,
         y: mouse.y - tempObject.pos1.y
+      };
+
+      // Create new IShape object, and push it to the objectStack later
+      const newShape: IShape = {
+        objectData: <EGPShapes.box>{ 
+          type: 'box' // Replace static object type with the matching type to the selected tool
+        }, 
+        position: [ 
+          tempObject.pos1, // Replace tempObject wth something else
+          tempObject.pos2
+        ], 
+        color: { 
+          r: 255, // Replace static values with values from a selection tool
+          g: 175, 
+          b: 175 
+        }, 
       }
-      console.log(tempObject);
+      console.log(newShape);
+
+      // Push to final IShape object to the object stack
+      objectStack.push(newShape);
+
+      // Empty the tempObject so it's ready for a new object
+      tempObject = {};
     }
 
   }
