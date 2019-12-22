cea2cdc6c2e6febf5cfd623827f1a8c77ef96900
DenseOriginal <48748922+DenseOriginal@users.noreply.github.com>
2019-12-22 22:33:36 +0100

    Installed ngx-color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
 import { EditorComponent } from './editor/editor.component';
 import { ToolSelectionComponent } from './editor/tool-selection/tool-selection.component';
+import { ObjectManipulatorComponent } from './editor/object-manipulator/object-manipulator.component';
+import { ColorChromeModule } from 'ngx-color/chrome';
 
 @NgModule({
   declarations: [
     AppComponent,
     EditorComponent,
-    ToolSelectionComponent
+    ToolSelectionComponent,
+    ObjectManipulatorComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    ColorChromeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
