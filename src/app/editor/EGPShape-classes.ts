b271ee480f8c7ac350b007c578fe1d2b4c1e36ba
DenseOriginal <48748922+DenseOriginal@users.noreply.github.com>
2019-12-27 18:46:01 +0100

    Added ellipse shape, and made the code pretty

--- src/app/editor/EGPShape-classes.ts
+++ src/app/editor/EGPShape-classes.ts
@@ -18,7 +18,7 @@ export class ShapeClass { // A base shapeClass that holds teh base information a
         this.id = id_
     }
 
-    public getType(): string{
+    public getType(): string {
         return this.type;
     }
 
@@ -26,11 +26,11 @@ export class ShapeClass { // A base shapeClass that holds teh base information a
         return this.id
     }
 
-    public display() {};
-    public compile(index: number): string {return ''};
-    public clicked(): boolean {return true};
-    public addPos(newPos: IPosition) {};
-    public setColor(newColor: IColor) {this.color = newColor;};
+    public display() { };
+    public compile(index: number): string { return '' };
+    public clicked(): boolean { return true };
+    public addPos(newPos: IPosition) { };
+    public setColor(newColor: IColor) { this.color = newColor; };
 }
 
 export namespace EGPObjects { // Namespace for all the different shapes
@@ -43,25 +43,25 @@ export namespace EGPObjects { // Namespace for all the different shapes
 
             // Add object styling here
 
-            
+
             // Fill the box the color it has
             this.p.fill(this.color.r, this.color.g, this.color.b);
-            
-            if(this.style.outline) {
+
+            if (this.style.outline) {
                 this.p.stroke(this.color.r, this.color.g, this.color.b);
                 this.p.strokeWeight(4);
                 this.p.noFill();
             } else {
                 this.p.noStroke();
             }
-            
+
             // If the object is selected outline it
-            if(this.selected) {this.p.stroke(100, 100, 255);}
-            
+            if (this.selected) { this.p.stroke(100, 100, 255); }
+
             // First check if it has a pos, if it doesn't have, don't draw anything
-            if(this.pos[0]) {
+            if (this.pos[0]) {
                 // Second check, if it has a pos and a widht and height
-                if(this.pos[1]) {
+                if (this.pos[1]) {
                     // Draw the shape using the measuments in the class
                     this.p.rect(
                         this.pos[0].x,
@@ -87,7 +87,112 @@ export namespace EGPObjects { // Namespace for all the different shapes
             }
 
             var scaledPos = [
-                { 
+                {
+                    x: scale(this.pos[0].x, 0, this.p.width, 0, 512),
+                    y: scale(this.pos[0].y, 0, this.p.width, 0, 512)
+                },
+                {
+                    x: scale(this.pos[1].x, 0, this.p.width, 0, 512),
+                    y: scale(this.pos[1].y, 0, this.p.width, 0, 512)
+                }
+            ];
+            scaledPos = [
+                {
+                    x: Math.floor(scaledPos[0].x + scaledPos[1].x / 2),
+                    y: Math.floor(scaledPos[0].y + scaledPos[1].y / 2)
+                },
+                {
+                    x: Math.floor(scaledPos[1].x),
+                    y: Math.floor(scaledPos[1].y)
+                }
+            ]
+
+            const objectString = `EGP:egpBox${this.style.outline ? 'Outline' : ''}(${index + 1}, vec2(${scaledPos[0].x}, ${scaledPos[0].y}), vec2(${scaledPos[1].x}, ${scaledPos[1].y}))`
+            const colorString = `EGP:egpColor(${index + 1}, vec3(${this.color.r}, ${this.color.g}, ${this.color.b}))`
+
+            return objectString + ' ' + colorString;
+        }; // Add a compile function here
+        clicked() {
+            // If the cursor is inside the shape return true;
+            if (
+                this.p.mouseX > this.pos[0].x && // If mouse.x is greater than the objects x cordinate
+                this.p.mouseY > this.pos[0].y && // If mouse.y is greater than the objects y cordinate
+                this.p.mouseX < this.pos[0].x + this.pos[1].x && // If mouse.x is less than the objects x cordinate plus the width
+                this.p.mouseY < this.pos[0].y + this.pos[1].y // If mouse.y is less than the objects y cordinate plus the height
+            ) return true; else return false;
+        };
+        addPos(newPos: IPosition) {
+            // Add a new pos, limit the amount if positions to 2
+            if (this.pos.length === 2 || this.pos.length > 2) return;
+            this.pos.push({
+                x: Math.round(newPos.x),
+                y: Math.round(newPos.y)
+            });
+
+            // If the array is filled with two position, check if the second pos is negative, and make it positive
+            if (this.pos.length === 2) {
+                // If the width or height is negative, make it positive, and move the x/y back the correct amount
+                if (this.pos[1].x < 0) { this.pos[0].x += this.pos[1].x; this.pos[1].x = Math.abs(this.pos[1].x) }
+                if (this.pos[1].y < 0) { this.pos[0].y += this.pos[1].y; this.pos[1].y = Math.abs(this.pos[1].y) }
+            };
+        };
+    };
+
+    // Ellipse shape, basicly the same code as box shape, but new display, clicked, compile function
+    export class ellipse extends ShapeClass { // Ellipse shape that extend the base shapeClass
+        constructor(type_: ShapeTypes, p_: p5, id_: number) {
+            super(type_, p_, id_);
+        }
+
+        display() { // Display function for box, that display the box 
+
+            // Add object styling here
+
+
+            // Fill the box the color it has
+            this.p.fill(this.color.r, this.color.g, this.color.b);
+
+            if (this.style.outline) {
+                this.p.stroke(this.color.r, this.color.g, this.color.b);
+                this.p.strokeWeight(4);
+                this.p.noFill();
+            } else {
+                this.p.noStroke();
+            }
+
+            // If the object is selected outline it
+            if (this.selected) { this.p.stroke(100, 100, 255); }
+
+            // First check if it has a pos, if it doesn't have, don't draw anything
+            if (this.pos[0]) {
+                // Second check, if it has a pos and a widht and height
+                if (this.pos[1]) {
+                    // Draw the shape using the measuments in the class
+                    this.p.rect(
+                        this.pos[0].x,
+                        this.pos[0].y,
+                        this.pos[1].x,
+                        this.pos[1].y
+                    );
+                } else {
+                    // If it only has one position
+                    // Draw the shape at the specified postion, and calculate the width and height
+                    this.p.rect(
+                        this.pos[0].x,
+                        this.pos[0].y,
+                        this.p.mouseX - this.pos[0].x,
+                        this.p.mouseY - this.pos[0].y
+                    );
+                }
+            }
+        };
+        compile(index: number): string {
+            const scale = (num, in_min, in_max, out_min, out_max) => {
+                return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+            }
+
+            var scaledPos = [
+                {
                     x: scale(this.pos[0].x, 0, this.p.width, 0, 512),
                     y: scale(this.pos[0].y, 0, this.p.width, 0, 512)
                 },
@@ -114,7 +219,7 @@ export namespace EGPObjects { // Namespace for all the different shapes
         }; // Add a compile function here
         clicked() {
             // If the cursor is inside the shape return true;
-            if(
+            if (
                 this.p.mouseX > this.pos[0].x && // If mouse.x is greater than the objects x cordinate
                 this.p.mouseY > this.pos[0].y && // If mouse.y is greater than the objects y cordinate
                 this.p.mouseX < this.pos[0].x + this.pos[1].x && // If mouse.x is less than the objects x cordinate plus the width
@@ -123,17 +228,17 @@ export namespace EGPObjects { // Namespace for all the different shapes
         };
         addPos(newPos: IPosition) {
             // Add a new pos, limit the amount if positions to 2
-            if(this.pos.length === 2 || this.pos.length > 2) return;
+            if (this.pos.length === 2 || this.pos.length > 2) return;
             this.pos.push({
                 x: Math.round(newPos.x),
                 y: Math.round(newPos.y)
             });
 
             // If the array is filled with two position, check if the second pos is negative, and make it positive
-            if(this.pos.length === 2){
+            if (this.pos.length === 2) {
                 // If the width or height is negative, make it positive, and move the x/y back the correct amount
-                if(this.pos[1].x < 0) { this.pos[0].x += this.pos[1].x; this.pos[1].x = Math.abs(this.pos[1].x) }
-                if(this.pos[1].y < 0) { this.pos[0].y += this.pos[1].y; this.pos[1].y = Math.abs(this.pos[1].y) }
+                if (this.pos[1].x < 0) { this.pos[0].x += this.pos[1].x; this.pos[1].x = Math.abs(this.pos[1].x) }
+                if (this.pos[1].y < 0) { this.pos[0].y += this.pos[1].y; this.pos[1].y = Math.abs(this.pos[1].y) }
             };
         };
     };
